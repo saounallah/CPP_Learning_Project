e9b0083b6f9308fc95b63c9789e31003475f2c6a
Saounallah <selmaaounallah@gmail.com>
2022-04-17 17:54:39 +0200

    Task 2 - Objectif 2 - C

--- src/aircraft_manager.cpp
+++ src/aircraft_manager.cpp
@@ -3,6 +3,23 @@
 
 bool AircraftManager::move()
 {
+    std::sort(aircrafts.begin(), aircrafts.end(),
+        [](std::unique_ptr<Aircraft>& aircraft1, std::unique_ptr<Aircraft>& aircraft2)
+        {
+            if (aircraft1->has_terminal() && aircraft2->has_terminal())
+            {
+                return aircraft1->get_fuel() < aircraft2->get_fuel();
+            }
+            if (aircraft1->has_terminal())
+            {
+                return true;
+            }
+            if (aircraft2->has_terminal())
+            {
+                return false;
+            }
+            return aircraft1->get_fuel() < aircraft2->get_fuel();
+        });
     aircrafts.erase(std::remove_if(aircrafts.begin(), aircrafts.end(),[](std::unique_ptr<Aircraft>& aircraft)
                     { return !aircraft->move() || aircraft->get_fuel() == 0; }),
                     aircrafts.end());
